4b6cb1929a58b1cb5ff1a2cc9e7e0bd1c978d573
CrimsonRavioli <84968886+CrimsonRavioli@users.noreply.github.com>
2025-08-31 14:41:40 +0100

    button now makes sound
    Move new script into appropriate folder.
    Added two audio clips for the button.
    Modified button visual script to also play sound.

--- Assets/Scripts/ButtonVisual.cs
+++ Assets/Scripts/ButtonVisual.cs
@@ -5,10 +5,16 @@ public class ButtonVisual : MonoBehaviour
     [SerializeField] GameObject _button;
     [SerializeField] Color buttonUpColor = Color.gray;
     [SerializeField] Color buttonDownColor = Color.red;
+
+    [SerializeField] AudioClip buttonDownSound;
+    [SerializeField] AudioClip buttonUpSound;
+
     MeshRenderer _buttonRenderer;
+    AudioSource _audioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         _buttonRenderer = _button.GetComponent<MeshRenderer>();
         _buttonRenderer.material.color = buttonUpColor;
     }
@@ -19,11 +25,15 @@ public class ButtonVisual : MonoBehaviour
         {
             _buttonRenderer.material.color = buttonDownColor;
             _button.transform.localPosition = new Vector3(0, 0.1f, 0);
+            _audioSource.clip = buttonDownSound;
+            _audioSource.Play();
         }
         else
         {
             _buttonRenderer.material.color = buttonUpColor;
             _button.transform.localPosition = new Vector3(0, .5f, 0);
+            _audioSource.clip = buttonUpSound;
+            _audioSource.Play();
         }
         
     }
